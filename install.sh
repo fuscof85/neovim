69d2cae4d5c1c09ba12a56842ac3b4c8327a1ca4
Francesco Fusco <francesco.fusco.085@gmail.com>
2025-09-04 09:17:11 +0200

    node required for some lsp languages

--- install.sh
+++ install.sh
@@ -1,2 +1,8 @@
+# nvim 
 curl -L -o nvim-linux-x86_64.tar.gz https://github.com/neovim/neovim/releases/download/nightly/nvim-linux-x86_64.tar.gz
 mkdir -p /opt/nvim && tar xzvf nvim-linux-x86_64.tar.gz -C /opt/nvim --strip-components=1
+
+# node, for some lsp languages
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+source ~/.bashrc
+nvm install 22
